09b8e306e99a30075aa6849e4f01d432d957d0f9
Muhammad Shehzad <m10351905@gmail.com>
2026-01-25 20:01:38 +0500

    Core_Java 1st

---
+++ VariablesDemo.java
@@ -0,0 +1,18 @@
+public class VariablesDemo {
+
+
+    static void main() {
+        int age;
+        String name;
+        
+        age = 20;
+        name = "Shehzad";
+
+//        System.out.println(STR."Hello, \{name}. You are \{age} old");
+        System.out.println("Hello," +name + " You are " + age +" old");
+
+        // CONSTANTS
+        final int CONSTANT_VALUE = 25;
+        
+    }
+}
